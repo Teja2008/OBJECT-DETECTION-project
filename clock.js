1188728fb6162aba92abc0d532c17122bc5c90a3
Teja2008 <86116952+Teja2008@users.noreply.github.com>
2023-01-31 20:11:40 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,12 @@
+function preload(){
+    img= loadImage('clock.jpg')
+}
+function setup(){
+    canvas= createCanvas(600,600)
+    canvas.center()
+    objectDetector= ml5.objectDetector('cocossd',modelLoaded)
+}
+function draw(){
+image(img,0,0,600,600)
+console.log("clock")
+}
